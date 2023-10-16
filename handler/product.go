08f2f7db09ed8d8d2203893a5ecce28745937751
balsamif <balsamif@student.21-school.ru>
2023-10-17 00:00:51 +0300

    End with intro

--- handler/product.go
+++ handler/product.go
@@ -4,6 +4,8 @@ import (
 	"gRPC/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Product struct {
@@ -21,6 +23,25 @@ func (pr *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 	if r.Method == http.MethodPost {
 		pr.postProduct(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		//we have ti get id from URL
+		regex := regexp.MustCompile("/([0-9]+)")
+		g := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, _ := strconv.Atoi(idString)
+		pr.l.Println("got id", id)
+
+		pr.updateProduct(id, rw, r)
 		return
 	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -43,4 +64,21 @@ func (pr *Product) postProduct(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable decode json", http.StatusInternalServerError)
 	}
 	data.AddProduct(lp)
+}
+
+func (pr *Product) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	pr.l.Println("Handle PUT Product")
+	lp := &data.Product{}
+	err := lp.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable decode json", http.StatusInternalServerError)
+	}
+	err = data.UpdateProduct(id, lp)
+	if err != nil {
+		if err == data.ErrProductNotFound {
+			http.Error(rw, "Product not found", http.StatusNotFound)
+			return
+		}
+		http.Error(rw, "Something went wrong", http.StatusBadRequest)
+	}
 }
